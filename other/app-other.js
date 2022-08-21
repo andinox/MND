60d1bd6fa8d66b3aca2057dc3f1723cb139ca81a
AnDiNoX <56691495+andinox@users.noreply.github.com>
2022-08-21 13:39:09 +0200

    Add files via upload

---
+++ other/app-other.js
@@ -0,0 +1,11 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+
+particlesJS
+particlesJS.load('particles-js', '../assets/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+});
+
+
+particlesJS.load('particles-js-night', '../assets/particles-night.json', function() {
+    console.log('callback - particles-night.json config loaded');
+});
