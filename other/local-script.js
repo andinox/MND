6ad44fc854bed5e1667478f6630b4bfe13b1599a
AnDiNoX <56691495+andinox@users.noreply.github.com>
2022-09-04 18:30:25 +0200

    end of publication

--- other/local-script.js
+++ other/local-script.js
@@ -1,12 +1,26 @@
 //global variable
 let data;
+let temps_tags = [];
 
-
+document.getElementById('up').addEventListener("click", function() {
+    main = document.getElementById('main');
+    main.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+      });
+})
 
 String.prototype.allspace = function() {
     let nb = 0;
     for (let i = 0; i < this.length; i++) nb+= this[i] == ' ' ? 1 : 0;
     return nb == this.length;
+}
+
+Array.prototype.combine = function() {
+    let all = "";
+    for (let i = 0; i < this.length; i++) all += String(this[i]).toUpperCase() +" ";
+    return all;
 }
 
 window.addEventListener('load', async function() {
@@ -15,6 +29,13 @@ window.addEventListener('load', async function() {
     start(url.get("poll"))
 })
 
+
+String.prototype.splice = function(idx, rem, str) {
+    if (this.length < 4) {
+        return this;
+    }
+    return this.slice(0, idx) + str + this.slice(idx + Math.abs(rem));
+};
 
 const search = document.getElementById('addbottom');
 search.addEventListener('click', function() {
@@ -25,6 +46,7 @@ search.addEventListener('click', function() {
     } else if (value.allspace()) {
         return;
     }
+    updatetags("add",value);
     croix = `${value} <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
                 <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
             </svg>`
@@ -32,21 +54,82 @@ search.addEventListener('click', function() {
     div = document.createElement('div');
     div.setAttribute('id','tag');
     div.innerHTML = croix;
+    div.setAttribute("data-name",value);
     div.addEventListener('click', function() {
-        deltags(this)
+        deltags(this);
+        updatetags("del",this.dataset.name);
     })
     bar = document.getElementById('allbar');
     bar.appendChild(div);
 
-
-
 })
 
+
+
+
+function updatetags(type,name) {
+    name = name.toUpperCase();
+    switch (type) {
+        case "del":
+            elements = document.querySelectorAll(`.element:not([data-tags*="${name}"])`);
+            elements.forEach(element => {
+                element.classList.remove("none");
+            });
+            break;
+        case "add":
+            elements = document.querySelectorAll(`.element:not([data-tags*="${name}"])`);
+            elements.forEach(element => {
+                element.classList.add("none");
+            });        
+    }
+    updatedate();
+}
+
+function updatedate() {
+    let allinmain = document.querySelectorAll('.main > *');
+    for (let i = 0; i < allinmain.length; i++) {
+        console.log(allinmain[i].getAttribute("id"));
+        if (allinmain[i].getAttribute("id") == "date") {
+            a = i+1;
+            while(allinmain[a].classList.contains('none') && allinmain[a+1] != undefined) {
+                a++;
+            }
+            if (allinmain[a].getAttribute("id") == "date" || a+1 == allinmain.length) {
+                allinmain[i].style.display = "none";
+            } else {
+                allinmain[i].style.display = "flex";
+            }
+        }
+    }
+}
 
 function deltags(event) {
     event.remove();
 }
 
+
+function short(data)  {
+    let copy_data = data;
+    let len = copy_data.length;
+    let temp_data = [];
+    let final_data = [];
+    for (let i = 0; i < data.length; i++) {
+        max = {"lore" : [0]};
+        len = copy_data.length;
+        for (let u = 0; u < len ; u++) {
+            if (max.lore[0] < copy_data[u].lore[0]) {
+                temp_data.push(max)
+                max = copy_data[u];
+            } else {
+                temp_data.push(copy_data[u])
+            }
+        }
+        final_data.push(max);
+        copy_data = temp_data;
+        temp_data = [];
+    }
+    return final_data;
+}
 
 function start(classtype) {    
     const header = document.getElementById('head');
@@ -75,12 +158,23 @@ function colab() {
 
 
 function publication() {
-    const cobal = data['Publications'];
-    console.log(cobal)
+    const cobal = short(data['Publications']);
     main = document.getElementById('main');
+    let temp_date = null;
     for (let i = 0; i < cobal.length; i++) {
+        if (temp_date != cobal[i].lore[0]) {
+           let dib = document.createElement('div');
+           dib.setAttribute('id','date');
+
+           let pdate = document.createElement('h1');
+           pdate.innerHTML = cobal[i].lore[0];
+           dib.appendChild(pdate);
+           main.appendChild(dib);
+           temp_date = cobal[i].lore[0];
+        } 
         let element = document.createElement('div');
         element.classList.add('element');
+        element.setAttribute('data-tags',cobal[i].lore.combine());
 
         let titre = document.createElement('h1');
         titre.innerHTML = cobal[i]["title"];
